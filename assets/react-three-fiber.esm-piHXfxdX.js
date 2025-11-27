df1c6433eb76777da246ff98c76896ffa2ebb5ee
Guinetik <guinetik@gmail.com>
2025-11-27 06:03:43 -0300

    Deployed Version: 2.0.6 at 2025-11-27T09:03:41.084Z

--- assets/react-three-fiber.esm-piHXfxdX.js
+++ assets/react-three-fiber.esm-piHXfxdX.js
@@ -1,4 +1,4 @@
-import{r as st,h as _y,j as Pi}from"./index-C0GTMcbo.js";/**
+import{r as st,h as _y,j as Pi}from"./index-Q2Dkhe1c.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
