09cd8bbcdc5a7be13e99c5224fa4b39326a65e44
Guinetik <guinetik@gmail.com>
2025-11-27 07:32:21 -0300

    Deployed Version: 2.0.11 at 2025-11-27T10:32:20.262Z

--- assets/react-three-fiber.esm-BFRJP57R.js
+++ assets/react-three-fiber.esm-BFRJP57R.js
@@ -1,4 +1,4 @@
-import{r as st,h as _y,j as Pi}from"./index-UIUtINQF.js";/**
+import{r as st,h as _y,j as Pi}from"./index-KgdxG6_W.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
